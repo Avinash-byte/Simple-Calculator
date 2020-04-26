cdd730409ba5f5384bd0538a2ba871da6bf3b6c8
Avinash Kumar <avinashky1996@outlook.com>
2020-04-26 15:55:59 +0530

    simple calculator first commit

---
+++ calc.js
@@ -0,0 +1,9 @@
+function update(value)
+{
+    document.getElementById("screen").value+=value;
+}
+
+function result()
+{
+    document.getElementById("screen").value=eval(document.getElementById("screen").value);
+}
